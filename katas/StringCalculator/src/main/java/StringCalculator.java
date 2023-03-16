8915c9cc3448e409e7d520a3ac32b1e632ea9a8c
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-03-16 14:44:37 +0000

    Empezando kata StringCalculator: Primer test correcto.

---
+++ katas/StringCalculator/src/main/java/StringCalculator.java
@@ -0,0 +1,6 @@
+public class StringCalculator {
+
+    public static String calculator(String numbers) {
+        return "";
+    }
+}
---
+++ katas/StringCalculator/src/test/java/TestStringCalculatorShould.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestStringCalculatorShould {
+    /*
+    * Make sure you only test for correct inputs. there is no need to test for invalid inputs for this kata.
+    - Use cases:
+        * 1. Given: empty string ("") When StringCalculator.calculator() Then return string 0.
+        * 2. Given: string with one number ("1") When StringCalculator.calculator() Then return same number string 1.
+        * 3. Given: string with two numbers ("1,2") When StringCalculator.calculator() Then return add(1,2) = string 3.
+        * 4.
+    */
+
+    @Test
+    public void returnAnEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.calculator(""), "");
+    }
+}
