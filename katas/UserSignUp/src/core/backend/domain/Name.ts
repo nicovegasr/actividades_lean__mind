e5d6eb28e4e530202c70301e1cf39dc59fd45145
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:24:17 +0100

    Name test(green): Name cannot be empty.

--- katas/UserSignUp/src/core/backend/domain/Name.ts
+++ katas/UserSignUp/src/core/backend/domain/Name.ts
@@ -2,6 +2,7 @@ export class Name {
   private constructor(private name: string) {  }
   
   public static createName(name: string): Name {
+    if (!name) { throw new Error("Name cannot be empty.")} 
     return new Name(name)
   }
 }
