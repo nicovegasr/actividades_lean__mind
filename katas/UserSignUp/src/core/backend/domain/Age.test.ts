1c0020583353ca77fa977dedb06c1a8208ae57cf
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:20:20 +0100

    Age test(red): Age must be an integer.

--- katas/UserSignUp/src/core/backend/domain/Age.test.ts
+++ katas/UserSignUp/src/core/backend/domain/Age.test.ts
@@ -2,7 +2,8 @@ import {Age} from "./Age";
 /** Approach: Inside-out
  * 1.- Age is created
  * 2.- Age under 13 cant be created -> throw Error
- */
+ * 3.- Age must be an integer. 
+*/
 describe('Age test.', () => {
 
   test('Age is created with factory method.', () => {
@@ -13,4 +14,9 @@ describe('Age test.', () => {
   test('Age under 13 thrown an error', () => {
     expect(() => Age.createAge(12)).toThrow("You have to be 13 years old or older to create an Age.")
   });
+
+  test('Not integer age thrown an error', () => {
+    expect(() => Age.createAge(12.5)).toThrow("Age must be an integer.")
+  });
+
 })
--- katas/UserSignUp/src/core/backend/domain/Age.ts
+++ katas/UserSignUp/src/core/backend/domain/Age.ts
@@ -4,6 +4,7 @@ export class Age {
   public static createAge(age: number): Age {
     const ageIsUnder13 = age < 13
     if (ageIsUnder13) { throw new Error("You have to be 13 years old or older to create an Age.") }
+    
     return new Age(age)
   }
 }
