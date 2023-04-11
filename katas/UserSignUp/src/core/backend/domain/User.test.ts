699490d66f31ab977b14b2b39ce4abe1c2bbb74a
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:40:39 +0100

    User test(red): Correct fields test will return an User.

--- katas/UserSignUp/src/core/backend/domain/User.test.ts
+++ katas/UserSignUp/src/core/backend/domain/User.test.ts
@@ -13,6 +13,7 @@ describe('User test.', () => {
     it('Empty Email will throw an Error.', () => {
       expect(() => User.createUser("Nico", 22, "") ).toThrow("Email is empty.")
     })
+  
     it('Non-format Email will throw an Error.', () => {
       expect(() => User.createUser("Nico", 22, "non-format@") ).toThrow("Email doesnt have the correct format.")
     })
@@ -22,22 +23,23 @@ describe('User test.', () => {
     it('Empty Name will throw an Error.', () => {
       expect(() => User.createUser("", 22, "nico@gmail.com") ).toThrow("Name cannot be empty.")
     })
-  
   })
 
   describe('Wrong Age test.', () => {
     it('Age under 13 will throw an Error.', () => {
       expect(() => User.createUser("Nico", 10, "nico@gmail.com") ).toThrow("You have to be 13 years old or older to create an Age.")
     })
+
     it('Float age will throw an Error.', () => {
       expect(() => User.createUser("Nico", 17.5, "nico@gmail.com") ).toThrow("Age must be an integer.")
     })
-  
   })
 
-  describe('Correct fields test.', () => {
-
-  
+  describe('Correct fields test will return an User.', () => {
+    it('User created with correct fields.', () => {
+      const user = User.createUser("Nico", 22, "nico@gmail.com")
+      expect(user).toBeInstanceOf(User)
+    })
   })
 
 })
