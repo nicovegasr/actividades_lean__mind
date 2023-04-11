0e9b9c29cc5759c47d05d50fdd6acf493365102d
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:22:25 +0100

    Name test(green): Name is created.

--- katas/UserSignUp/src/core/backend/domain/Name.ts
+++ katas/UserSignUp/src/core/backend/domain/Name.ts
@@ -2,6 +2,6 @@ export class Name {
   private constructor(private name: string) {  }
   
   public static createName(name: string): Name {
-    throw new Error("Not implemented")
+    return new Name(name)
   }
 }
