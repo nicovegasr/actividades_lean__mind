b220382346d1932a4fc7610827679805e7cf5267
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:36:49 +0100

    User test(red): Wrong Email (Empty and non format) will throw an Error.

---
+++ katas/UserSignUp/src/core/backend/domain/User.test.ts
@@ -0,0 +1,33 @@
+import {User} from "./User"
+
+/** Approach: Inside-out
+ * 1.- Wrong Email (Empty or not format) will throw an Error
+ * 2.- Empty Name will throw an Error
+ * 3.- Wrong Age (under 13 or float) will throw an Errorç.
+ * 4.- Correct fields will create an user.
+*/
+
+describe('User test.', () => {
+
+  describe('Wrong Email test.', () => {
+    it('Empty Email will throw an Error.', () => {
+      expect(() => User.createUser("Nico", 22, "") ).toThrow("Email is empty.")
+    })
+  })
+
+  describe('Wrong Name test.', () => {
+
+  
+  })
+
+  describe('Wrong Age test.', () => {
+
+  
+  })
+
+  describe('Correct fields test.', () => {
+
+  
+  })
+
+})
--- katas/UserSignUp/src/core/backend/domain/User.ts
+++ katas/UserSignUp/src/core/backend/domain/User.ts
@@ -1,3 +1,11 @@
-export class User {
+import {Age} from "./Age"
+import {Name} from "./Name"
+import {Email} from "./Email"
 
+export class User {
+  private constructor(public name: Name, public age: Age, public Email: Email) {  }
+  
+  public static createUser(name: string, age: number, email: string): User {
+    throw new Error("Not implemented.")
+  }
 }
