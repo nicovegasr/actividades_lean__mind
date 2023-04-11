71b6dafd4a659b9fcbbdd8ea9f765791c72b8699
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:32:48 +0100

    Email test(greenn): Non-format email should throw an error.

--- katas/UserSignUp/src/core/backend/domain/Email.ts
+++ katas/UserSignUp/src/core/backend/domain/Email.ts
@@ -2,7 +2,9 @@ export class Email {
   private constructor(public email: string) {  }
 
   public static createEmail(email: string): Email {
+    const incorrectFormat = !(email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
     if (!email) { throw new Error("Email is empty.") }
+    if (incorrectFormat) { throw new Error("Email doesnt have the correct format.")}
     return new Email(email)
   }
 }
