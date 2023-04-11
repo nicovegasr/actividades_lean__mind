ad0dfa5fd52f8bd6dbadd89eb786f9513729d9b9
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-04-11 08:18:45 +0100

    Age test(green): Age under 13 must throw an error.

--- katas/UserSignUp/src/core/backend/domain/Age.ts
+++ katas/UserSignUp/src/core/backend/domain/Age.ts
@@ -2,6 +2,8 @@ export class Age {
   private constructor(private age: number) {  }
   
   public static createAge(age: number): Age {
-    return new Age(age)
+    const ageIsNotUnder13 = age > 13
+    if (ageIsNotUnder13) { return new Age(age) }
+    throw new Error("You have to be 13 years old or older to create an Age.")
   }
 }
