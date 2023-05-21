e33259648be53a1a1ef9d84475eeca65f24c2898
Nicolas Vegas Rodriguez <alu0101321745@ull.edu.es>
2023-05-21 14:22:39 +0100

    test(put_a_book_in_my_bookshelf): :rotating_light: Reed - :white_check_mark: Green - :recycle: Refactor.

---
+++ ejercicios/LearningMocks/src/main/java/org/example/NormalBookShelf.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface NormalBookShelf {
+    void add_normal_book(Book book);
+    Book get_normal_book(Book book);
+}
--- ejercicios/LearningMocks/src/test/java/org/example/LibraryServicesTest.java
+++ ejercicios/LearningMocks/src/test/java/org/example/LibraryServicesTest.java
@@ -1,6 +1,9 @@
 package org.example;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+import static org.mockito.Mockito.*;
 
 public class LibraryServicesTest {
     /* Use cases:
@@ -9,5 +12,15 @@ public class LibraryServicesTest {
      * 3. Try to put an existent book in my bookshelf -> Error: BOOK_ALREADY_EXISTS.
      * 4. Try to get a nonexistent book from by bookshelf -> Error: NONEXISTENT_BOOK.
      */
+    @Test
+    public void put_a_book_in_my_bookshelf() {
+        Book book = Book.NORMAL_BOOK;
+        LibraryServices libraryServices = new LibraryServices();
+        NormalBookShelf normalBookShelf = mock(NormalBookShelf.class);
 
+        libraryServices.put_a_book(book, normalBookShelf);
+
+        verify(normalBookShelf).add_normal_book(book);
+
+    }
 }
