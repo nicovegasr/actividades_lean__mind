f921ff7056c710a7559d63e3d7f0f76f738bd834
Nicolas Vegas Rodriguez <alu0101321745@ull.edu.es>
2023-05-21 17:53:18 +0100

    :sparkles: Value object age created.

--- ejercicios/DomainDriverDesign-Chapter-1/src/main/java/org/domain/value_objects/Age.java
+++ ejercicios/DomainDriverDesign-Chapter-1/src/main/java/org/domain/value_objects/Age.java
@@ -1,4 +1,20 @@
 package org.domain.value_objects;
 
+import io.vavr.control.Either;
+import org.domain.warnings.AgeWarning;
+
 public class Age {
+    private final Integer age;
+    private Age(Integer age) {
+        this.age = age;
+    }
+    public static Either<AgeWarning, Age> createAge(Integer age) {
+        if (age <= 13) {
+            return Either.left(AgeWarning.AGE_UNDER_LIMIT);
+        }
+        return Either.right(new Age(age));
+    }
+    public Integer getAge() {
+        return age;
+    }
 }
