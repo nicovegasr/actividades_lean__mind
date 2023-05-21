e32e1ea14dc509e9d50244260af9b22acff3a4cb
Nicolas Vegas Rodriguez <alu0101321745@ull.edu.es>
2023-05-21 15:10:52 +0100

    test(try_to_put_an_existent_book_in_my_bookshelf): :rotating_light: Reed - :white_check_mark: Green - :recycle: Refactor.

--- ejercicios/LearningMocks/src/main/java/org/example/NormalBookShelf.java
+++ ejercicios/LearningMocks/src/main/java/org/example/NormalBookShelf.java
@@ -2,5 +2,6 @@ package org.example;
 
 public interface NormalBookShelf {
     void add_normal_book(Book book);
+    boolean book_exists(Book book);
     Book get_normal_book(Book book);
 }
--- ejercicios/LearningMocks/src/test/java/org/example/LibraryServicesTest.java
+++ ejercicios/LearningMocks/src/test/java/org/example/LibraryServicesTest.java
@@ -1,26 +1,44 @@
 package org.example;
 
-import org.junit.Test;
 
+import io.vavr.control.Either;
+import org.junit.jupiter.api.*;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class LibraryServicesTest {
     /* Use cases:
      * 1. Put a new normal book in my bookshelf.
-     * 2. Get a normal book from my bookshelf.
-     * 3. Try to put an existent book in my bookshelf -> Error: BOOK_ALREADY_EXISTS.
+     * 2. Try to put an existent book in my bookshelf -> Error: BOOK_ALREADY_EXISTS.
+     * 3. Get a normal book from my bookshelf.
      * 4. Try to get a nonexistent book from by bookshelf -> Error: NONEXISTENT_BOOK.
      */
+    Book book;
+    LibraryServices libraryServices;
+    NormalBookShelf normalBookShelf;
+    @BeforeEach
+    public void setUp() {
+        normalBookShelf = mock(NormalBookShelf.class);
+        libraryServices = new LibraryServices(normalBookShelf);
+    }
     @Test
     public void put_a_book_in_my_bookshelf() {
-        Book book = Book.NORMAL_BOOK;
-        LibraryServices libraryServices = new LibraryServices();
-        NormalBookShelf normalBookShelf = mock(NormalBookShelf.class);
+        book = Book.NORMAL_BOOK;
 
-        libraryServices.put_a_book(book, normalBookShelf);
+        Either<BookWarning, Book> response = libraryServices.put_a_book(book);
 
         verify(normalBookShelf).add_normal_book(book);
+        assertTrue(response.isRight());
+    }
+    @Test
+    public void try_to_put_an_existent_book_in_my_bookshelf() {
+        book = Book.NORMAL_BOOK;
 
+        when(normalBookShelf.book_exists(book)).thenReturn(true);
+        Either<BookWarning, Book> response = libraryServices.put_a_book(book);
+
+        verify(normalBookShelf, never()).add_normal_book(book);
+        assertEquals(response.getLeft(), BookWarning.BOOK_ALREADY_EXISTS);
     }
 }
